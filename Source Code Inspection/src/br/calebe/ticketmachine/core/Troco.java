bd6b14feb7396310e9bb6e1c23026a994c7ae223
31316034 <31316034@087873-P33S105.macklabs.br>
2014-09-17 22:29:48 -0300

    teste

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -71,7 +71,7 @@ class Troco {
             PapelMoeda ret = null;
             for (int i = 6; i >= 0 && ret != null; i++) {
                 if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
+//                    ret = troco.papeisMoeda[i];
                     troco.papeisMoeda[i] = null;
                 }
             }
